c42650bff11044144a08609c74d259d9823b962f
manuhuth <s6mahuth@uni-bonn.de>
2023-03-30 12:54:21 +0200

    Add matrix scalar multiplication.

---
+++ R/multiplyMatrixScalarDS.R
@@ -0,0 +1,16 @@
+#' @title multiplyMatrixScalarDS
+#' @title Multiply Matrix and Scalar
+#' @description The function is used to multiply a matrix and a scalare.
+#' @param matrix The matrix to be multiplied.  Must be a string with the name of the matrix on the server side.
+#' @param scalar The scalar to be multiplied. Must be a single number.
+#' @return A matrix representing the product of the two inputs
+#' @export
+multiplyMatrixScalarDS <- function(matrix, scalar){
+
+  matrix <- eval(parse(text=matrix), envir = parent.frame())
+  scalar <- eval(parse(text=scalar), envir = parent.frame())
+
+
+  product <- matrix * scalar
+  return(product)
+}
